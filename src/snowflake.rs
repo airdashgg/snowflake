e0d86eadfcbf223a3668c632078104a03a2628d0
fooooooooooooooo <34515228+fooooooooooooooo@users.noreply.github.com>
2023-08-07 05:07:16 -0700

    serialize as string

--- src/snowflake.rs
+++ src/snowflake.rs
@@ -77,7 +77,7 @@ impl Display for Snowflake {
 impl Serialize for Snowflake {
   fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
   where S: serde::Serializer {
-    serializer.serialize_u64(self.value())
+    serializer.serialize_str(&self.value().to_string())
   }
 }
 
