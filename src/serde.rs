efec212280ddb3d0213222a9e91ceec919bd4a76
fooooooooooooooo <34515228+fooooooooooooooo@users.noreply.github.com>
2023-09-29 10:57:27 -0700

    deserialize from string

--- src/serde.rs
+++ src/serde.rs
@@ -1,4 +1,5 @@
-use serde::Serialize;
+use serde::{Deserialize, Serialize};
+use serde_aux::field_attributes::deserialize_number_from_string;
 
 use crate::Snowflake;
 
@@ -6,5 +7,14 @@ impl Serialize for Snowflake {
   fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
   where S: serde::Serializer {
     serializer.serialize_str(&self.value().to_string())
+  }
+}
+
+impl<'de> Deserialize<'de> for Snowflake {
+  fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+  where D: serde::Deserializer<'de> {
+    let value: u64 = deserialize_number_from_string(deserializer)?;
+
+    Ok(Self::from_value(value))
   }
 }
